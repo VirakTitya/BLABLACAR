9d7332adfde6515984cab0fd4c9aea3c98a3ec59
Panha Viraktitya <viraktitya.panha@student.cadt.edu.kh>
2026-02-11 20:37:22 +0700

    BLA-001 - Filter Rides (correction)

--- services/location_service.dart
+++ services/location_service.dart
@@ -1,4 +1,5 @@
 import '/data/dummy_data.dart';
+
 import '../model/ride/locations.dart';
 import '../model/ride/ride.dart';
 
@@ -9,4 +10,20 @@ import '../model/ride/ride.dart';
 class LocationsService {
   static const List<Location> availableLocations =
       fakeLocations; // TODO for now fake data
+
+  static List<Ride> filterByDeparture(Location departure){
+    return fakeRides.where((ride) => ride.departureLocation == departure).toList();
+  }
+
+  static List<Ride> filterBySeatRequested(int seatRequested){
+    return fakeRides.where((ride) => ride.availableSeats >= seatRequested).toList();
+  }
+
+  static List<Ride> filterBy({Location? departure, int? seatRequested}){
+    return fakeRides.where((ride) {
+      final matchDeparture = departure == null || ride.departureLocation == departure;
+      final matchSeat = seatRequested == null || ride.availableSeats >= seatRequested;
+      return matchDeparture && matchSeat;
+    }).toList();
+  }
 }
